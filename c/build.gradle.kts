5860b21cce02615445aa582a2fe7a6d7b28a20a5
lloydmeta <lloydmeta@gmail.com>
2020-06-12 10:17:55 +0900

    Fix readme, add project c
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

---
+++ c/build.gradle.kts
@@ -0,0 +1,17 @@
+dependencies {
+  implementation("org.scala-lang:scala-library:2.12.11")
+  testImplementation(project(":b", "tests"))
+  testImplementation("org.scalatest:scalatest_2.12:3.1.2")
+  testImplementation("org.scalatestplus:junit-4-12_2.12:3.1.2.0")
+}
+
+configurations.create("tests") {
+  extendsFrom(configurations.testImplementation.get())
+}
+tasks.register<Jar>("testJar") {
+  archiveClassifier.set("tests")
+  from(sourceSets.test.get().output)
+}
+artifacts {
+  add("tests", tasks.named("testJar"))
+}
---
+++ c/src/test/scala/com/beachape/BSpec.scala
@@ -0,0 +1,12 @@
+package com.beachape
+
+class BSpec extends _0ASpecLike {
+
+  it("should yo") {
+    assert(yo == "yo")
+  }
+
+  it("should lol") {
+    assert(Lol.lol == "lol")
+  }
+}
